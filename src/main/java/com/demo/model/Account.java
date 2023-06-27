94f53f25539c111d61a5ad93ef0b323f4c6db0bc
minhcong213274 <gathidungru@gmail.com>
2023-06-27 20:08:10 +0700

    27/6/20h

--- src/main/java/com/demo/model/Account.java
+++ src/main/java/com/demo/model/Account.java
@@ -9,11 +9,14 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String email;
     private String password;
     private String name;
     private String age;
     private String gender;
+    @Column(unique = true)
+    private String phone;
     private String image;
     private Boolean status;
     @ManyToOne
@@ -26,16 +29,39 @@ public class Account {
     public Account() {
     }
 
-    public Account(String email, String password, String name, String age, String gender, String image, Boolean status, Location location, Role role) {
+    public Account(String email, String password, String phone) {
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public Account(String email, String password, String name, String age, String phone, String gender, String image, Boolean status, Location location, Role role) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.age = age;
         this.gender = gender;
+        this.phone = phone;
         this.image = image;
         this.status = status;
         this.location = location;
         this.role = role;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
     }
 
     public Long getId() {
