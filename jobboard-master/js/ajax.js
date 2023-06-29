63bd62e9df9c859816c2cad30a6ad0524bd5b1a7
minhcong213274 <gathidungru@gmail.com>
2023-06-29 09:59:14 +0700

    10h sang

--- jobboard-master/js/ajax.js
+++ jobboard-master/js/ajax.js
@@ -1,56 +1,35 @@
-$(document).ready(function () {
-    showListJob();
-    showListLocation()
-});
-
-function showListJob() {
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/job",
-        success: function (data) {
-            console.log(data);
-            let jobList = "";
-            for (let i = 0; i < data.length; i++) {
-                jobList += `<li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
-            <a href="job-single.html"></a>
-            <div class="job-listing-logo">
-              <img src="${data[i].image}" alt="Free Website Template by Free-Template.co" class="img-fluid">
-            </div>
-
-            <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
-              <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
-                <h2>${data[i].title}</h2>
-                <strong>${data[i].company.name}</strong>
-              </div>
-              <div class="job-listing-location mb-3 mb-sm-0 custom-width w-25">
-                <span class="icon-room"></span> ${data[i].job_location.name}, ${data[i].job_region}
-              </div>
-              <div class="job-listing-meta">
-                <span class="badge badge-danger">${data[i].job_type}</span>
-              </div>
-            </div>
-          </li>`
-            }
-            document.getElementById("job-listing").innerHTML = jobList;
-        }
-    })
-}
-
-
-function showListLocation() {
-   let listLocation = `<select class="selectpicker" data-style="btn-white btn-lg" data-width="100%"
-                                        data-live-search="true" title="Select Region" >`;
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/location",
-        success: function (data) {
-            console.log(data);
-            for (let i = 0; i < data.length; i++){
-                listLocation += `<option value="${data[i].id}">${data[i].name}</option>`;
-            }
-            listLocation += `</select>`
-            console.log(listLocation);
-            document.getElementById("test").innerHTML = listLocation
-        }
-    })
-}
+// $(document).ready(function () {
+//     showListJob();
+// });
+// function showListJob() {
+//     $.ajax({
+//         type: "GET",
+//         url: "http://localhost:8080/job",
+//         success: function (data) {
+//             console.log(data);
+//             let jobList = "";
+//             for (let i = 0; i < data.length; i++) {
+//                 jobList += `<li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+//             <a href="job-single.html"></a>
+//             <div class="job-listing-logo">
+//               <img src="${data[i].image}" alt="Free Website Template by Free-Template.co" class="img-fluid">
+//             </div>
+//
+//             <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
+//               <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
+//                 <h2>${data[i].title}</h2>
+//                 <strong>${data[i].company.name}</strong>
+//               </div>
+//               <div class="job-listing-location mb-3 mb-sm-0 custom-width w-25">
+//                 <span class="icon-room"></span> ${data[i].job_location.name}, ${data[i].job_region}
+//               </div>
+//               <div class="job-listing-meta">
+//                 <span class="badge badge-danger">${data[i].job_type}</span>
+//               </div>
+//             </div>
+//           </li>`
+//             }
+//             document.getElementById("job-listing").innerHTML = jobList;
+//         }
+//     })
+// }
